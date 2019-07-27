fa7fbd646a437aae033b307d5c3d208755f4f0e8
Fatih <fatih47154@gmail.com>
2019-07-27 22:54:09 +0300

    socket.io redis adapter

--- src/socketApi.js
+++ src/socketApi.js
@@ -2,8 +2,18 @@ const socketio = require('socket.io');
 const io = socketio();
 
 const socketApi = {
-    io              // io: io
+    io // io: io
 };
+
+/**
+ *  Redis Adapter
+ */
+
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URI,
+    port: process.env.REDIS_PORT
+}));
 
 io.on('connection', socket => {
     console.log('a user logged in');
